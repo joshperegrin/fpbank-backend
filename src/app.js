5f65b678bc7839a4afc0ac2547e6cf84121bdbd6
Joshua Peregrin <johnjoshuaperegrin2@gmail.com>
2025-05-07 19:54:30 +0800

    feat(route): add account routes for user account creation - Routes for opening an account with file upload. - Integrates account creation with validation middleware.

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const authRoutes = require("./routes/auth.routes.js")
+const accountRoutes = require("./routes/account.routes.js")
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use('/auth', authRoutes)
+app.use('/account', accountRoutes)
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`)
---
+++ src/routes/account.routes.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const multer = require("multer")
+const path = require("path")
+const { openAccountController } = require("../controllers/account.controller.js");
+const openAccountMiddleware = require("../middleware/openaccount.middleware.js");
+
+const router = express.Router();
+const storage = multer.diskStorage({
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname)
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9) + ext
+    cb(null, uniqueSuffix)
+  }
+})
+
+const upload = multer({storage: storage});
+
+router.post("/", upload.single('validid'), openAccountMiddleware, openAccountController);
+
+module.exports = router;
+
