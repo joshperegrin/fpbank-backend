13dbe953134d9dd601b6c7a604b20efb52a8ac6d
Joshua Peregrin <johnjoshuaperegrin2@gmail.com>
2025-05-13 17:48:49 +0800

    feat(route): route for transaction history of a user - Added a new route `/transactions` to retrieve user transactions with pagination. - Implemented validation for `page` and `limit` query parameters using `express-validator`. - Applied error handling for invalid query parameters. - Integrated the `getTransactionsController` to handle transaction retrieval.

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
+const { query, validationResult } = require('express-validator');
 const multer = require("multer")
 const path = require("path")
-const { openAccountController, getBalanceController } = require("../controllers/account.controller.js");
+const { openAccountController, getBalanceController, getTransactionsController } = require("../controllers/account.controller.js");
 const openAccountMiddleware = require("../middleware/openaccount.middleware.js");
 const authMiddleware = require("../middleware/auth.middleware.js")
 
@@ -16,8 +17,24 @@ const storage = multer.diskStorage({
 
 const upload = multer({storage: storage});
 
+const validations = [
+  query('page').optional().isInt({ min: 1 }).toInt(),
+  query('limit').optional().isInt({ min: 1, max: 100 }).toInt(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ 
+        errors: errors.array() 
+      });
+    }
+    next()
+  }
+]
+
+
 router.post("/", upload.single('validid'), openAccountMiddleware, openAccountController);
 router.get("/balance", authMiddleware, getBalanceController)
+router.get("/transactions", authMiddleware, validations, getTransactionsController)
 
 module.exports = router;
 
