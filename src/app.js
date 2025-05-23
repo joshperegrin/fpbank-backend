7fea370ae8ab05e822a44536caf235bcd30af33a
Joshua Peregrin <johnjoshuaperegrin2@gmail.com>
2025-05-24 07:49:53 +0800

    fix(network): enable cors

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors')
 const authRoutes = require("./routes/auth.routes.js")
 const accountRoutes = require("./routes/account.routes.js")
 const transferRoutes = require("./routes/transfer.routes.js")
@@ -9,6 +10,10 @@ const PORT = process.env.PORT;
 app.use('/auth', authRoutes)
 app.use('/account', accountRoutes)
 app.use('/transfer', transferRoutes)
+
+app.use(cors({
+  origin: 'http://localhost:8100'
+}))
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`)
