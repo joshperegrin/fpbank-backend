d8f33de4759e2cddd4f6a8e9d32e095a89b56b13
Joshua Peregrin <johnjoshuaperegrin2@gmail.com>
2025-05-26 00:39:44 +0800

    fix: cors issues

--- src/app.js
+++ src/app.js
@@ -3,17 +3,16 @@ const cors = require('cors')
 const authRoutes = require("./routes/auth.routes.js")
 const accountRoutes = require("./routes/account.routes.js")
 const transferRoutes = require("./routes/transfer.routes.js")
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT;
 
+app.use(cors())
 app.use('/auth', authRoutes)
 app.use('/account', accountRoutes)
 app.use('/transfer', transferRoutes)
 
-app.use(cors({
-  origin: 'http://localhost:8100'
-}))
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on http://localhost:${PORT}`)
